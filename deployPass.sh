df3fa3c2809f316b70c85d0035d3fe983f2bddb7
Ali Tayarani <ali@broadinstitute.org>
2011-11-03 16:41:04 -0400

    Bug fixes

--- deployPass.sh
+++ deployPass.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
 hosts=$@
+jarfile="./ipmitool/SMCIPMITool.jar"
+if [ ! -f $jarfile ]; then
+    echo "Error: ${jarfile} Not Found"
+    exit 1
+fi
+
 echo "Enter password:"
 stty -echo
 read PASS
 stty echo
 
-for host in hosts; do 
-	addUser="java -jar /home/radon01/ali/scratch/smc/ipmitool/SMCIPMITool.jar"
-	addUser="$addUser $(host $HOST | awk '{print $NF}') ADMIN ADMIN"
+for host in $hosts; do 
+	addUser="java -jar $jarfile"
+	addUser="$addUser $(host $host | awk '{print $NF}') ADMIN ADMIN"
 	addUser="$addUser user add 3 root $PASS 4"
 	
-	delUser="ipmitool -H $HOST -U root -P $PASS user set"
+	delUser="ipmitool -H $host -U root -P $PASS user set"
 	delUser="$delUser password 2 $[$RANDOM*$RANDOM*$RANDOM]"
 	
 	$addUser
